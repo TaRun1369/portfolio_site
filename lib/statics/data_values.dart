303b7b251dba7478e965c5376062db9694f85db5
TaRun1369 <tarunsantani2003@gmail.com>
2023-11-24 22:34:34 +0530

    portfolio 1st

---
+++ lib/statics/data_values.dart
@@ -0,0 +1,127 @@
+class DataValues {
+  // About App
+  static const String appName = 'Tarun Santani';
+  static const String appNameLong = 'Tarun Santani - Personal Website';
+  static const String appDescription =
+      'Hello there, I\'m Tarun Santani. This is my personal website, which contains public information that anyone can find on the internet.';
+  static const String appDeveloper = 'Tarun Santani';
+  static const String appPackage = 'dev.tarunsantani';
+  static const String appVersion = '';
+  static const String appBuildNumber = '1';
+
+  static Uri githubURL = Uri.parse('https://github.com/tarun1369');
+  static Uri linkedinURL = Uri.parse('https://linkedin.com/in/');
+  static Uri instagramURL = Uri.parse('https://instagram.com/tarunsantani13');
+
+  // Header
+  static const String headerGreetings = 'Hello, World! I\'m';
+  static const String headerName = 'Tarun Santani';
+  static const String headerTitle = 'Student | Developer';
+
+  // Navigation Bar
+  static const String navBarAboutMe = 'About Me';
+  static const String navBarEducation = 'Education';
+  // static const String navBarExperience = 'Experience';
+  static const String navBarVolunteering = 'Volunteering';
+  // static const String navBarTechNotes = 'TechNotes';
+  static const String navBarContactMe = 'Contact Me';
+
+  // About me
+  static const String aboutMeTitle = 'About Me';
+  static const String aboutMeDescription =
+      'This website includes my public details, which anyone can find on the internet. So if you are looking for my personal information, such as my bio, education, experience, and other things that may make you wonder, feel free to scroll up and down to find out about me.';
+  static const String aboutMeBiographyTitle = 'Biography';
+  static const String aboutMeBiographyDescription =
+      'Hi there! I\'m a Engineering student, solution developer, and club member. I believe that personal growth and learning are essential for success, and I have dedicated my life to achieving both. '
+      'Because of that, I\'m always looking to learn new things and apply them to whatever I\'m doing. '
+      'Technology is always igniting my passion for the positive impact it can have, and I\'m excited to continue growing my skills and knowledge, so I started my career as a techie. '
+      'In addition to my studies, I\'m also passionate about giving back to various communities and helping others, which is why I volunteer my time to help out whenever I can, and I believe that this work is vital in helping to create a better future for everyone.';
+  static const String aboutMeFullNameTitle = 'Full Name';
+  static const String aboutMeFullNameDescription = 'Tarun Sunil Santani';
+  static const String aboutMeFnLTitle = 'First and Last Name';
+  static const String aboutMeFnLDescription = 'Tarun Santani';
+  static const String aboutMeDobTitle = 'Birthday';
+  static const String aboutMeDobDescription = 'Dec 20, 2003';
+  static const String aboutMeLanguageTitle = 'Languages';
+  static const String aboutMeLanguageDescription = 'Hindi (Native)\n'
+      'English (Professional)';
+  static const String aboutMeNationalityTitle = 'Nationality';
+  static const String aboutMeNationalityDescription = 'Indian';
+  static const String aboutMeGenderTitle = 'Gender';
+  static const String aboutMeGenderDescription = 'Male (He/Him/His)';
+  static const String aboutMeLocationTitle = 'Current Locations';
+  static const String aboutMeLocationDescription = 'Pune';
+  static const String aboutMeStudentTitle = 'Engineering Student';
+  static const String aboutMeStudentDescription =
+      'I study computing technologies as a student by focusing on learning how computer systems work, from the physical (hardware) to the apps and programs (software) that users interact with, and I learn all about critical thinking, problem-solving, and analytical research, regardless of whether I apply all the computing principles to solve problems in scenarios.';
+  static const String aboutMeDeveloperTitle = 'Solution Developer';
+  static const String aboutMeDeveloperDescription =
+      'I use various tools for creating and implementing innovative solutions to problems in order to accomplish particular goals. I employ a variety of techniques, including software development alongside app development, as well as design and develop structures such as UI/UX, graphics, data management, and other analytical methods with a focus on business points.';
+  static const String aboutMeVolunteerTitle = 'Club Volunteer';
+  static const String aboutMeVolunteerDescription =
+      'I\'ve been a volunteer for many years and have had the opportunity to make a difference in my social and technical Clubs. Volunteering has allowed me to learn more about the people and things around me while also making a positive difference in the lives of those I serve, and by volunteering, I would love to help others reach their life goals.';
+  static const String aboutMeHobbiesTitle = 'Hobbies';
+  static const String aboutMeHobbiesDescription = 'Learning New Things\n'
+      'Watching Movies & TV Shows\n'
+      'Listening to Music & Dancing\n'
+      'Table Tennis';
+  static const String aboutMeGoalTitle = 'Goal';
+  static const String aboutMeGoalDescription =
+      'To grow personally and professionally, while making a positive impact on the community.';
+
+  // Education
+  static const String educationTitle = 'Education';
+  static const String educationDescription =
+      'My educational background is not specific in any particular way and has come from various places. So, here are my school, undergraduate institution, and other learning platforms that I\'ve been involving for education.';
+  static const String educationOrg1Title =
+      'Lal Bahadur Shastri Dhyanpeeth jr. college';
+  static const String educationOrg1Course1Name = 'HSC';
+  static const String educationOrg1Course1Grade = '';
+  static const String educationOrg1Course1Year = '2021';
+  static const String educationOrg1Course2Name = ' ';
+  static const String educationOrg1Course2Grade = '';
+  static const String educationOrg1Course2Year = '';
+  static const String educationOrg1Course3Name = '';
+  static const String educationOrg1Course3Year = '';
+  static const String educationOrg1Course3Year2 = '';
+
+  static const String educationOrg2Title = 'PICT';
+  static const String educationOrg2Course1Name = 'Computer Engineering';
+  static const String educationOrg2Course1Grade = '';
+  static const String educationOrg2Course1Year = '';
+  static const String educationOrg2Course2Name = '';
+  static const String educationOrg2Course2Grade = '';
+  static const String educationOrg2Course2Year = '';
+  static const String educationOrg2Course3Name = '';
+  static const String educationOrg2Course3Grade = '8.95 CGPA';
+  static const String educationOrg2Course3Year = '2021 - 2025';
+  // static const String educationBanner = 'These Are Some Of My Other';
+  // static const String educationBannerTitle = 'Online Learning Platforms';
+  // static const String educationBannerWeb =
+  //     'Click here to find out more about my online learning platforms.';
+
+  // Volunteering
+  static const String volunteeringTitle = 'Roles And Responsibilities';
+  static const String volunteeringDescription =
+      'Volunteering is an amazing way for me to give back to communities and make a difference. It\'s also a terrific opportunity to meet new people, learn new skills, and gain valuable experience. I do volunteer work for many different social and technical organizations. These are more focused organizations where I am presently volunteering.';
+  static const String volunteeringOrg1Title = 'Pictoreal';
+  static const String volunteeringOrg1Role = 'Treasurer';
+  static const String volunteeringOrg1Years = 'August 2023 - Present';
+  static const String volunteeringOrg1Vales =
+      'Managing and  monitoring flow of Money is my job';
+  static const String volunteeringOrg2Title = 'PASC';
+  static const String volunteeringOrg2Role = 'Member';
+  static const String volunteeringOrg2Years = '2021 - Present';
+  static const String volunteeringOrg2Vales =
+      'Technical Club adding techy spice in life';
+  // static const String volunteeringBanner = 'Find more about my';
+  // static const String volunteeringBannerTitle = 'volunteering';
+  // static const String volunteeringBannerWeb =
+  //     'Visit volunteer.dileepabandara.dev for more details >>';
+
+  // Footer
+  static const String copyright =
+      "Copyright ©2024 Tarun Santani. All Rights Reserved.";
+
+  static const String builtWith = "Built with Flutter and GitHub Pages.";
+}
